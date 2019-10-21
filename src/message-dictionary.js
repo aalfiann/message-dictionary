b1ecf4c731f321f941f8388cae447255c927251f
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2019-10-21 21:19:17 +0700

    - remove __dirname

--- src/message-dictionary.js
+++ src/message-dictionary.js
@@ -49,7 +49,7 @@ class MessageDictionary {
      * @return {string}
      */
     getFilename() {
-        return path.join(__dirname,this.dirPath + '/' + this.namespace + '/' + this.locale + '.js');
+        return path.join(this.dirPath + '/' + this.namespace + '/' + this.locale + '.js');
     }
 
     /**
